010acda0676ced1c32e3edcfea51a9ba1d27ca00
jornl <jorn@lemika.net>
2022-11-14 18:09:16 +0100

    Update TestCase.php

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -8,6 +8,13 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+    
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->withoutVite();
+    }
 
     protected function signIn($user = null)
     {
